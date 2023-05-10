cf275761bb17c645758648028adbcf70c9d0396b
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2023-05-10 19:50:19 +0000

    base demo content

--- demo001/Program.cs
+++ demo001/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Start of demo");
+
+List<string> APIM_Terms = new() { "APIM", "API Management" };
+List<string> SB_Terms = new() { "Service Bus", "ServiceBus" };
+List<string> LA_Terms = new() { "Logic App", "LogicApp" };
+List<string> Functions_Terms = new() { "function" };
+
+
+List<string> Integration_Terms = new() { "event", "Functions", "AIS", "Integration", "API", "serverless" };
+
+
+
+foreach (var item in Integration_Terms)
+{
+    Console.WriteLine(item);    
+}
+
+
+Console.WriteLine("End of demo");
+
